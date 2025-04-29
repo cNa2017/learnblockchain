3233a617be6d0a75e59c5ba8813b9d725d058048
lianxiang <cna100110@163.com>
2025-04-29 14:40:15 +0800

    添加get函数

--- day2/Counter.sol
+++ day2/Counter.sol
@@ -2,10 +2,14 @@
 pragma solidity ^0.8.18;
 
 contract Counter{
-    uint public count;
+    uint public count = 0;
 
     function add() public   {
         count++;
+    }
+
+    function get () public view returns (uint _count) {
+        _count = count;
     }
 
 }
